5f79fe5e7b81b7b1a45cd472abbc241534e40f35
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-03 23:50:47 +0530

    Added: Snake and Ladder game start with single player at position zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,9 @@
 package com.bridgelab.snakeladder;
 
 public class SnakeAndLadder {
+	static int player=0;
 public static void main(String[] args) {
 	System.out.println("Well come to Snake and Ladder Game");
+	System.out.println(" Game start player at position  "+player);
 }
 }
