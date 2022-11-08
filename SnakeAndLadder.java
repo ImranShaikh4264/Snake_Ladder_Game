7be9f3e2ab6054ed80f61010f967b1e80d02b9b6
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-08 22:36:18 +0530

    Added: Player reach 100 and win the game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,22 +1,53 @@
 package com.bridgelab.snakeladder;
 
-// UC-3 Player checks for option no play snake or ladder.
+// UC-4 Repeat till player position is 100.
 public class SnakeAndLadder {
-	static int player = 0;
+	public static int checkSnakeLadder(int pos) {
+		int position=pos;
+		switch(position) {
+		case 4:
+			position=14;
+			break;
+		case 42:
+			position=42;
+			break;
+		case 24:
+			position=84;
+			break;
+		case 78:
+			position=99;
+			break;
+		case 62:
+			position=19;
+			break;
+		case 49:
+			position=13;
+			break;
+		case 87:
+			position=36;
+			break;
+		case 98:
+			position=79;
+			break;
+		default:
+			position=position;
+		}
+		return position;
+	}
 
 	public static void main(String[] args) {
+		int position = 0;
 		System.out.println("Well come to Snake and Ladder Game");
-		System.out.println(" Game start player at position  " + player);
-		int option = (int) (Math.random() * (3) + 1);
-		if (option == 1) {
-			System.out.println("No play");
-		} else if (option == 2) {
-			int dice = (int) (Math.random() * (6) + 1);
-			System.out.println(" player is playing dice : " + dice);
-		} else {
-			int dice = (int) (Math.random() * (6) + 1);
-			System.out.println(" player is playing dice : " + dice);
+		System.out.println(" Game start player at position  " + position);
+		int counter=0;
+		for (position=1;position<100;position++) {
+		int dice = (int) (Math.random() * (6) + 1);
+		counter++;
+		position=position+dice;
+		position=checkSnakeLadder(position);
 		}
-
+		System.out.println("Player won the game");
+		System.out.println("Player position  " +position);
+		System.out.println("Number of time dice throw  "+counter);
 	}
 }
