9546d1e946bf387005b2aeed8b2e635560abe587
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-10 16:16:09 +0530

    Added: Player position after each dice and number of times dice rolls

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,28 +6,63 @@ public class SnakeAndLadder {
 		int position=pos;
 		switch(position) {
 		case 4:
+			System.out.println("Ladder");
 			position=14;
+			System.out.println("position :"+position);
 			break;
-		case 42:
-			position=42;
+		case 8:
+			System.out.println("Ladder");
+			position=30;
+			System.out.println("position :"+position);
 			break;
-		case 24:
-			position=84;
+		case 28:
+			System.out.println("Ladder");
+			position=74;
+			System.out.println("position :"+position);
+			break;
+		case 36:
+			System.out.println("Ladder");
+			position=70;
+			System.out.println("position"+position);
+			break;
+		case 44:
+			System.out.println("Ladder");
+			position=78;
+			System.out.println("position :"+position);
 			break;
 		case 78:
+			System.out.println("Ladder");
 			position=99;
+			System.out.println("position :"+position);
 			break;
-		case 62:
-			position=19;
+		case 32:
+			System.out.println("Snake");
+			position=10;
+			System.out.println("position :"+position);
+			break;
+		case 35:
+			System.out.println("Snake");
+			position=6;
+			System.out.println("position :"+position);
 			break;
 		case 49:
+			System.out.println("Snake");
 			position=13;
+			System.out.println("position :"+position);
+			break;
+		case 97:
+			System.out.println("Snake");
+			position=75;
 			break;
 		case 87:
-			position=36;
+			System.out.println("Snake");
+			position=67;
+			System.out.println("position :"+position);
 			break;
 		case 98:
+			System.out.println("Snake");
 			position=79;
+			System.out.println("position :"+position);
 			break;
 		default:
 			position=position;
@@ -40,18 +75,18 @@ public class SnakeAndLadder {
 		System.out.println("Well come to Snake and Ladder Game");
 		System.out.println(" Game start player at position  " + position);
 		int counter=0;
-		for (position=0;position<100;position++) {
+		for (position=0;position<100;) {
 		int dice = (int) (Math.random() * (6)+1);
 		position=position+dice;
 		counter++;
-		System.out.println("player position  "+position);
+		System.out.println("player position :"+position);
 		position=checkSnakeLadder(position);
 		if (position>100) {
 			position=position-dice;
 			
 		}
 		}
-		System.out.println("Player position final  " +position);
+		System.out.println("Player position final : " +position);
 		System.out.println("Player won the game");
 		System.out.println("Number of time dice throw  "+counter);
 	}
