3d46beb7c39f0ae1c149f814cb6a7ad18ef6c5fd
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-19 17:24:55 +0530

    Added: Snake and Ladder game with Two Player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,93 +1,130 @@
 package com.bridgelab.snakeladder;
 
-// UC-6 Repeat till player position is  at exact 100 and find number of time dice throw to win game.
+// UC-7 Snake and Ladder game with two player.
 public class SnakeAndLadder {
+	public static int position1 = 0;
+	public static int position2 = 0;
+	static boolean condition = true;
+
 	public static int checkSnakeLadder(int pos) {
-		int position=pos;
-		switch(position) {
+		int position = pos;
+		switch (position) {
 		case 4:
 			System.out.println("Ladder");
-			position=14;
-			System.out.println("position :"+position);
+			position = 14;
+			System.out.println("position :" + position);
 			break;
 		case 8:
 			System.out.println("Ladder");
-			position=30;
-			System.out.println("position :"+position);
+			position = 30;
+			System.out.println("position :" + position);
 			break;
 		case 28:
 			System.out.println("Ladder");
-			position=74;
-			System.out.println("position :"+position);
+			position = 74;
+			System.out.println("position :" + position);
 			break;
 		case 36:
 			System.out.println("Ladder");
-			position=70;
-			System.out.println("position"+position);
+			position = 70;
+			System.out.println("position" + position);
 			break;
 		case 44:
 			System.out.println("Ladder");
-			position=78;
-			System.out.println("position :"+position);
+			position = 78;
+			System.out.println("position :" + position);
 			break;
 		case 78:
 			System.out.println("Ladder");
-			position=99;
-			System.out.println("position :"+position);
+			position = 99;
+			System.out.println("position :" + position);
 			break;
 		case 32:
 			System.out.println("Snake");
-			position=10;
-			System.out.println("position :"+position);
+			position = 10;
+			System.out.println("position :" + position);
 			break;
 		case 35:
 			System.out.println("Snake");
-			position=6;
-			System.out.println("position :"+position);
+			position = 6;
+			System.out.println("position :" + position);
 			break;
 		case 49:
 			System.out.println("Snake");
-			position=13;
-			System.out.println("position :"+position);
+			position = 13;
+			System.out.println("position :" + position);
 			break;
 		case 97:
 			System.out.println("Snake");
-			position=75;
+			position = 75;
 			break;
 		case 87:
 			System.out.println("Snake");
-			position=67;
-			System.out.println("position :"+position);
+			position = 67;
+			System.out.println("position :" + position);
 			break;
 		case 98:
 			System.out.println("Snake");
-			position=79;
-			System.out.println("position :"+position);
+			position = 79;
+			System.out.println("position :" + position);
 			break;
 		default:
-			position=position;
+			position = position;
 		}
 		return position;
 	}
 
+	public static void player1() {
+		System.out.println(" Game start player at position  " + position1);
+		int counter1 = 0;
+		if (position1 < 100) {
+			int dice = (int) (Math.random() * (6) + 1);
+			position1 = position1 + dice;
+			counter1++;
+			System.out.println("player1 position :" + position1);
+			position1 = checkSnakeLadder(position1);
+			if (position1 > 100) {
+				position1 = position1 - dice;
+			}
+
+		} else if (position1 == 100) {
+			System.out.println("Player 1 win");
+			condition = false;
+
+		}
+	}
+
+	public static void player2() {
+		System.out.println(" Game start player2 at position  " + position2);
+		int counter2 = 0;
+		if (position2 < 100) {
+			int dice = (int) (Math.random() * (6) + 1);
+			position2 = position2 + dice;
+			counter2++;
+			System.out.println("player2 position :" + position2);
+			position2 = checkSnakeLadder(position2);
+			if (position2 > 100) {
+				position2 = position2 - dice;
+			}
+		} else if (position2 == 100) {
+			System.out.println("Palayer 2 win");
+			condition = false;
+		}
+	}
+
 	public static void main(String[] args) {
-		int position = 0;
+
 		System.out.println("Well come to Snake and Ladder Game");
-		System.out.println(" Game start player at position  " + position);
-		int counter=0;
-		for (position=0;position<100;) {
-		int dice = (int) (Math.random() * (6)+1);
-		position=position+dice;
-		counter++;
-		System.out.println("player position :"+position);
-		position=checkSnakeLadder(position);
-		if (position>100) {
-			position=position-dice;
-			
+		int counter = 0;
+
+		while (condition) {
+			if (counter % 2 != 0) {
+				player1();
+				counter++;
+			} else {
+				counter++;
+				player2();
+			}
 		}
-		}
-		System.out.println("Player position final : " +position);
-		System.out.println("Player won the game");
-		System.out.println("Number of time dice throw  "+counter);
 	}
 }
